99d4bc62ab3eeb977e7aca8ce959b8bc71b5990a
celestial-applet-1261 <zihan991@gmail.com>
2021-03-22 13:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-    return kittens.push(name)
+    return kittens.push(name);
 }
 
 function destructivelyPrependKitten(name){
-    return kittens.unshift(name)
+    return kittens.unshift(name);
 }
 
 function destructivelyRemoveLastKitten(){
-    return kittens.pop()
+    return kittens.pop();
 }
 
 function destructivelyRemoveFirstKitten(){
-    return kittens.shift()
+    return kittens.shift();
 }
 
 function appendKitten(name){
-    return [...kittens, name]
+    return [...kittens, name];
 }
 
 function prependKitten(name){
-    return [name, ...kittens]
+    return [name, ...kittens];
 }
 
 function removeLastKitten(){
-    return kittens.slice(0,kittens.length-1)
+    return kittens.slice(0,kittens.length-1);
 }
 
 function removeFirstKitten(){
-    return kittens.slice(1)
+    return kittens.slice(1);
 }
